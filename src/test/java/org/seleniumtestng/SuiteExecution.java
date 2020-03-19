7f97feb594408594ccc414990a0fedd25a71b3f9
sravan reddy <sravanreddy234@gmail.com>
2020-03-19 16:38:22 +0530

    fewModifications_SuiteExecution
    fewModifications_SuiteExecution

--- src/test/java/org/seleniumtestng/SuiteExecution.java
+++ src/test/java/org/seleniumtestng/SuiteExecution.java
@@ -6,12 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GroupsExample {
-	
+public class SuiteExecution {
 	public WebDriver driver;
 	@Test(groups= {"one","two"})
 	public void tc01LaunchURL() {
-		System.out.println("in 1st method");
 		System.setProperty("webdriver.chrome.driver","D:\\Selenium Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -19,7 +17,6 @@ public class GroupsExample {
 	}
 	@Test(groups= {"one"})
 	public void tc02VerifyLaunchPage() {
-		System.out.println("in 2nd method");
 		String expectedTitle = " GTPL Bank Home Page ";
 		String pageTitle = driver.getTitle();
 		Assert.assertTrue(driver.findElement(By.xpath("//h2[text() = 'Gtpl Bank']")).isDisplayed(), "Launch page displayed");
@@ -27,14 +24,13 @@ public class GroupsExample {
 	}
 	@Test(groups= {"one","two"})
 	public void tc03EnterCredentials() {
-		System.out.println("in 3rd method");
 		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr251028");
 		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("mErAbUz");
 		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
 	}
 	@Test(groups= {"one","two"})
 	public void tc04VerifyHyperlinks() {
-		System.out.println("in 4th method");
+		
 		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Log out']")).isDisplayed(),"Logout is available");
 		System.out.println("Logout is not available");
 		driver.findElement(By.xpath("//a[text()='Log out']")).click();
@@ -42,7 +38,6 @@ public class GroupsExample {
 	}
 	@Test(groups= {"two"})
 	public void tc05AcceptPopup() {
-		System.out.println("in 5th method");
 		String popUpText = driver.switchTo().alert().getText();
 		System.out.println(popUpText);
 		driver.switchTo().alert().accept();
@@ -50,10 +45,8 @@ public class GroupsExample {
 	}
 	@Test(groups= {"one","two"})
 	 public void tc06closeExecution() {
-		System.out.println("in 6th method");
 		System.out.println("in closeExecution method");
 		 driver.quit();
 		 System.out.println("Execution stopped");
 	 }
-	
 }
